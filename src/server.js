199c74da8abd812ca6c2018f7559c46e1c8ac88b
Misael Alberto <misael.alberto@outlook.com>
2023-07-08 00:08:53 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+    console.log(`App listening on porta ${PORT}`)
 })
